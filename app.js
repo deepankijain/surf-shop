bf38dbe63489c3e3bf616d15abf92767f54e67e1
Deepanki Jain <deepankijain@gmail.com>
2020-08-20 20:03:01 +0530

    Add reviews route

--- app.js
+++ app.js
@@ -4,9 +4,10 @@ const path = require('path');
 const cookieParser = require('cookie-parser');
 const logger = require('morgan');
 
-const indexRouter = require('./routes/index');
-const usersRouter = require('./routes/users');
-const postsRouter = require('./routes/posts');
+const index = require('./routes/index');
+const users = require('./routes/users');
+const reviews = require('./routes/reviews');
+const posts = require('./routes/posts');
 
 const app = express();
 
@@ -22,9 +23,10 @@ app.use(express.urlencoded({
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
-app.use('/', indexRouter);
-app.use('/users', usersRouter);
-app.use('/posts', postsRouter);
+app.use('/', index);
+app.use('/users', users);
+app.use('/posts', posts);
+app.use('/posts/:id/reviews', reviews);
 
 // catch 404 and forward to error handler
 app.use(function (req, res, next) {
